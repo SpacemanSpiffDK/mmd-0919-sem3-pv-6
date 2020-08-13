5e32f8de55577bfe5d5a5f3ee40f02b1ac048fc5
Dan Høegh <dan.hoegh@gmail.com>
2020-08-13 09:36:29 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// JS by Dan Høegh
+// UCN MMD 2020
+
+// Setting options in a json object
+let options = {
+    "controls": false, 
+    "autoplay": true, 
+    "preload": "auto", 
+    "muted": true
+};
+
+// try the different json-objects here
+videojs('video1', options);
